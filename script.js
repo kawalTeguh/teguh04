a8bc5a16f0e2ccdc0ed7fc08fa8e21840a11a3ae
kawalTeguh <152688328+kawalTeguh@users.noreply.github.com>
2024-02-15 19:25:48 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-const modeToggle = document.getElementById("mode-toggle");
-const body = document.body;
+function animation(span) {
+	span.className = "turn";
+	setTimeout(function () {
+		span.className = "";
+	}, 700);
+}
 
-modeToggle.addEventListener("click", () => {
-	if (body.getAttribute("data-bs-theme") === "dark") {
-		body.setAttribute("data-bs-theme", "light"); // Ganti ke mode terang
-	} else {
-		body.setAttribute("data-bs-theme", "dark"); // Ganti ke mode gelap
-	}
-});
+function jam() {
+	setInterval(function () {
+		var waktu = new Date();
+		var jam = document.getElementById("jam");
+		var hours = waktu.getHours();
+		var minutes = waktu.getMinutes();
+		var seconds = waktu.getSeconds();
+
+		if (waktu.getHours() < 10) {
+			hours = "0" + waktu.getHours();
+		}
+		if (waktu.getMinutes() < 10) {
+			minutes = "0" + waktu.getMinutes();
+		}
+		if (waktu.getSeconds() < 10) {
+			seconds = "0" + waktu.getSeconds();
+		}
+		jam.innerHTML = "<span>" + hours + "</span>" + "<span>" + minutes + "</span>" + "<span>" + seconds + "</span>";
+
+		var spans = jam.getElementsByTagName("span");
+		animation(spans[2]);
+		if (seconds == 0) animation(spans[1]);
+		if (minutes == 0 && seconds == 0) animation(spans[0]);
+	}, 1000);
+}
+
+jam();
